ca94ff3a95fe52d48568c6de6018e6c806b4cb2e
Marcos Ernesto Planos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2024-08-16 13:25:21 +0200

    feat: Update SideBar component with new design and functionality

--- src/components/layout/SideBar/SideBar.tsx
+++ src/components/layout/SideBar/SideBar.tsx
@@ -27,62 +27,68 @@ export default function SideBar() {
   if (isLoading) return <div>Loading...</div>;
   if (!boards || boards.length === 0) return <div>No boards found</div>;
   return (
-    <div className="hidden w-full sm:max-w-[260px] shadow-right lg:max-w-[300px] sm:flex flex-col border-r border-border/10 relative z-20 bg-gradient-to-r from-background/5 to-background-light/20 backdrop-blur-sm">
-      <div className="flex w-full items-center p-5 border-b border-border/10 shadow-bottom justify-between bg-gradient-to-b from-background/5 to-background-light/20">
+    <div className="hidden w-full sm:max-w-[260px] lg:max-w-[300px] sm:flex flex-col border-r border-border/10 relative z-2 ">
+      <div className="flex w-full items-center p-5 border-b border-border/10 shadow-bottom justify-between bg-gradient-to-br from-background/10 to-background-light/20">
         <UserBtn />
       </div>
-      <div className="overflow-scroll">
-        <ScrollShadow className="w-full h-full">
-          <Tabs
-            selectedKey={selectedBoardId?.toString()}
-            aria-label="Boards"
-            isVertical
-            onSelectionChange={handleSelectionChange}
-            fullWidth
-            variant="light"
+      <div className="flex w-full flex-col flex-grow bg-gradient-to-r from-background/5 to-background-light/20 backdrop-blur-sm shadow-right">
+        <div className="overflow-scroll">
+          <ScrollShadow className="w-full h-full">
+            <Tabs
+              selectedKey={selectedBoardId?.toString()}
+              aria-label="Boards"
+              isVertical
+              onSelectionChange={handleSelectionChange}
+              fullWidth
+              variant="light"
+              color="primary"
+              classNames={{
+                tabList: 'gap-4 w-full relative rounded-none pl-0 py-4 pr-4',
+                cursor: 'w-full rounded-l-none rounded-r-full',
+                tab: 'h-12 justify-start',
+                tabContent: 'w-full overflow-hidden font-semibold text-base',
+              }}
+            >
+              {boards.map((board) => (
+                <Tab
+                  key={board.id}
+                  id={String(board.id)}
+                  title={
+                    <div className="flex items-center overflow-hidden text-base gap-2">
+                      <span className="text-base">{svgBoard}</span>
+                      <span className="overflow-hidden overflow-ellipsis">
+                        {board.title}
+                      </span>
+                    </div>
+                  }
+                />
+              ))}
+            </Tabs>
+          </ScrollShadow>
+        </div>
+        <Divider className="w-[80%] mx-auto" />
+        <button
+          className="w-full flex items-center justify-start p-3 gap-2 text-base font-semibold text-primary"
+          onClick={() => onOpen()}
+        >
+          {svgAddBoard} + Create New Board
+        </button>
+        <div className="flex p-3 items-center justify-start mt-auto">
+          <Switch
+            size="md"
             color="primary"
-            classNames={{
-              tabList: 'gap-4 w-full relative rounded-none pl-0 py-4 pr-4',
-              cursor: 'w-full rounded-l-none rounded-r-full',
-              tab: 'h-12 justify-start',
-              tabContent: 'w-full overflow-hidden font-semibold text-base',
-            }}
-          >
-            {boards.map((board) => (
-              <Tab
-                key={board.id}
-                id={String(board.id)}
-                title={
-                  <div className="flex items-center overflow-hidden text-base gap-2">
-                    <span className="text-base">{svgBoard}</span>
-                    <span className="overflow-hidden overflow-ellipsis">
-                      {board.title}
-                    </span>
-                  </div>
-                }
-              />
-            ))}
-          </Tabs>
-        </ScrollShadow>
+            startContent={svgSun}
+            endContent={svgMoon}
+            isSelected={theme === 'light'}
+            onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          ></Switch>
+        </div>
+        <AddBoard
+          isOpen={isOpen}
+          onOpenChange={onOpenChange}
+          onClose={onClose}
+        />
       </div>
-      <Divider className="w-[80%] mx-auto" />
-      <button
-        className="w-full flex items-center justify-start p-3 gap-2 text-base font-semibold text-primary"
-        onClick={() => onOpen()}
-      >
-        {svgAddBoard} + Create New Board
-      </button>
-      <div className="flex p-3 items-center justify-start mt-auto">
-        <Switch
-          size="md"
-          color="primary"
-          startContent={svgSun}
-          endContent={svgMoon}
-          isSelected={theme === 'light'}
-          onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-        ></Switch>
-      </div>
-      <AddBoard isOpen={isOpen} onOpenChange={onOpenChange} onClose={onClose} />
     </div>
   );
 }
